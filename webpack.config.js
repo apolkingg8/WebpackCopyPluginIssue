30aa24e54480df5aead9951dc280e128457fc064
apolkingg8 <apolkingg8@gmail.com>
2020-06-10 22:32:22 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,36 @@ let path = require('path')
 let isDev = process.env['NODE_ENV'] !== 'production'
 
 let config = {
+    target: "electron-renderer",
     entry: path.join(__dirname, 'source', 'foo.ts'),
     output: {
         path: path.join(__dirname, 'packed'),
         filename: "foo.js",
+    },
+    mode: 'production',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: "ts-loader",
+                    options: {
+
+                    }
+                }],
+            },
+            {
+                test: /\.(css|sass)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+                loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
+            }
+        ]
     },
     plugins: [
         new WebpackCopyPlugin({
