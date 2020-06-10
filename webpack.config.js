f0b360c8c7c1568c9b527a299ebd0c7b7a7cadc0
apolkingg8 <apolkingg8@gmail.com>
2020-06-10 22:29:10 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+let webpack = require('webpack')
+let WebpackCopyPlugin = require('copy-webpack-plugin')
+let path = require('path')
+let isDev = process.env['NODE_ENV'] !== 'production'
+
+let config = {
+    entry: path.join(__dirname, 'source', 'foo.ts'),
+    output: {
+        path: path.join(__dirname, 'packed'),
+        filename: "foo.js",
+    },
+    plugins: [
+        new WebpackCopyPlugin({
+            patterns: [{
+                from: path.join(__dirname, 'source', 'foo.ts'),
+                to: path.join(__dirname, 'packed', 'foo.ts'),
+            }, {
+                from: path.join(__dirname, 'source', 'dir'),
+                to: path.join(__dirname, 'packed', 'dir'),
+                toType: 'dir',
+            }]
+        })
+    ]
+}
+
+module.exports = config
